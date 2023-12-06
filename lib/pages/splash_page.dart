abf5fa749e9e7edd471d10475084e9a754b248a7
teddinata <teddinata@users.noreply.github.com>
2023-12-06 11:52:38 +0700

    fix: splash screen update

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sportcenter/theme.dart';
-// timer
 import 'dart:async';
 
 class SplashPage extends StatefulWidget {
@@ -9,14 +8,30 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  String _loadingText = 'Loading 0%';
+
   @override
   void initState() {
-    // TODO: implement initState
-    Timer(
-      Duration(seconds: 3),
-      () => Navigator.pushNamed(context, '/sign-in'),
-    );
     super.initState();
+    Timer.periodic(
+      Duration(milliseconds: 700), // adjust the interval as needed
+      (Timer timer) {
+        setState(() {
+          final int percentage =
+              int.parse(_loadingText.split(' ')[1].replaceAll('%', ''));
+          if (percentage == 0) {
+            _loadingText = 'Loading 30%';
+          } else if (percentage == 30) {
+            _loadingText = 'Loading 75%';
+          } else if (percentage == 75) {
+            _loadingText = 'Loading 100%';
+          } else {
+            timer.cancel();
+            Navigator.pushNamed(context, '/sign-in');
+          }
+        });
+      },
+    );
   }
 
   @override
@@ -24,15 +39,47 @@ class _SplashPageState extends State<SplashPage> {
     return Scaffold(
       backgroundColor: backgroundColor1,
       body: Center(
-          child: Container(
-        width: 130,
-        height: 150,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/img-splash.png'),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 130,
+              height: 150,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/img-splash.png'),
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            Text(
+              _loadingText,
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Make sure your internet connection is stable.',
+              style: TextStyle(
+                color: secondaryTextColor,
+                fontWeight: regular,
+                fontSize: 14,
+              ),
+            ),
+            Text(
+              'Close the application and open it again if there is an error.',
+              style: TextStyle(
+                color: secondaryTextColor,
+                fontWeight: regular,
+                fontSize: 14,
+              ),
+            )
+          ],
         ),
-      )),
+      ),
     );
   }
 }
